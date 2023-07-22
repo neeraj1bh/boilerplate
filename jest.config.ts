39c55f6e5a1a4290e4e4da84c95b240c49ad283a
Neeraj Bhatt <neeraj1bh@gmail.com>
2023-07-23 00:30:05 +0530

    feat: added prettier, jest, git workflows and mocks

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': '@/__mocks__/styleMock.js',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '@/__mocks__/fileMock.js',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
